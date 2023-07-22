c14b44eabd670cfa0c6c521e1f79a3b8a28646da
quite <quitetaken@gmx.de>
2023-07-22 14:30:57 +0200

    prepate projectile shooting
    No projectiles added, just the code

--- Assets/Scripts/LevelLogic/Tower.cs
+++ Assets/Scripts/LevelLogic/Tower.cs
@@ -1,7 +1,10 @@
 using Characters;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
+using static UnityEngine.GraphicsBuffer;
 
 namespace Towers
 {
@@ -13,18 +16,20 @@ namespace Towers
         public int Range ;
 
         [SerializeField]
-        internal int damage;
+        public int damage;
         
         [SerializeField]
-        internal float attackSpeed;
+        public float attackSpeed;
         [SerializeField]
-        internal float travelTime; //Maybe, maybe not? Projectiles could always just go straight, to make it easy
+        public float TravelSpeed;
         
         internal GameObject currentTarget;
         
         [SerializeField]
         public int TargetPriority;
         internal Turret TurretName;
+
+        protected Projectile projectile;
 
         #endregion
 
@@ -37,7 +42,7 @@ namespace Towers
 
         protected virtual void Shoot()
         {
-            bool KilledTarget = currentTarget.GetComponent<Character>().TakeDamage(damage);
+            bool KilledTarget = currentTarget.GetComponent<Characters.Character>().TakeDamage(damage);
 
             // Only relevant for gun-type towers (they only switch target on kill)
             if (KilledTarget)
@@ -45,9 +50,9 @@ namespace Towers
         }
 
         // Start is called before the first frame update
-        void Start()
+        void Awake()
         {
-            
+            projectile = this.GetComponent<Projectile>();
         }
 
         // Update is called once per frame
@@ -56,8 +61,21 @@ namespace Towers
             SelectTarget();
             if (currentTarget != null && Time.deltaTime >= attackSpeed)
             {
+                AnimateProjectile();
                 Shoot();
             }
+        }
+
+        protected void AnimateProjectile()
+        {
+            if (projectile == null)
+                return;
+
+            var direction = this.transform.position - currentTarget.transform.position;
+            var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+            Projectile newProjectile = Instantiate(projectile, this.transform.position, Quaternion.AngleAxis(angle, Vector3.forward));
+            newProjectile.DefineTarget(currentTarget.transform.position, TravelSpeed);
         }
 
         #endregion
